6ebe32d36b06b5b0d17bb0c6d8f5249d6ce67cf1
MAGA1314 <garciaagudelomiguelangel@gmail.com>
2024-02-15 09:31:10 -0500

    clase_4_final

---
+++ app/src/main/java/co/nelson/appuno/PantallaDos.java
@@ -0,0 +1,80 @@
+package co.nelson.appuno;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class PantallaDos extends AppCompatActivity {
+
+   TextView nombreUsuario;
+   Button btn1,btn2;
+   String Tag = "Prueba";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nombreUsuario = (TextView) findViewById(R.id.usuario);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nombreUsuario.setText("Presioné Boton Uno");
+
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nombreUsuario.setText("Presioné Boton Dos");
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(Tag,"Estoy en OnStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(Tag,"Estoy en OnResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(Tag,"Estoy en OnPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(Tag,"Estoy en OnStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(Tag,"Estoy en OnRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(Tag,"Estoy en OnDestroy");
+    }
+}
